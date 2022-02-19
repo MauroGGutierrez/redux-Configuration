fc85639ca310446a3559380933af8751c5be2c2d
Mauro Gutierrez <yoguiimaster@gmail.com>
2022-02-19 10:03:49 -0300

    se agrego status, ahora esta bien actualizado la configuracion

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import { useDispatch, useSelector } from "react-redux";
 import { setOffDarkMode, setOnDarkMode } from "./redux/actions/themeActions";
 
 function App() {
-  const { darkMode } = useSelector((state) => state.darkMode); // lo que va despues del punto de state sera el nombre de la const
+  // const { darkMode } = useSelector((state) => state.darkMode); // lo que va despues del punto de state sera el nombre de la const
+  const darkMode = useSelector((state) => state.darkMode); // state = store global
   const dispatch = useDispatch();
   return (
     <>
-      <h1>
+      {/* <h1>
         {darkMode === true
           ? "DarkMode ON"
           : darkMode === false
           ? "darkMode OFF"
           : "No Seteado"}
-      </h1>
+      </h1> */}
+      <h1>{darkMode.status ? 'DarkMode ON' : 'DarkMode OFF'}</h1>
       <button onClick={() => dispatch(setOnDarkMode())}>
         Habilitar DarkMode
       </button>
       <button onClick={() => dispatch(setOffDarkMode())}>
-        deshabilitar DarkMode
+        deshabilitar DarkMode 
       </button>
     </>
   );
--- src/redux/actions/themeActions.js
+++ src/redux/actions/themeActions.js
@@ -3,12 +3,15 @@ import actionsTypes from "./actionsTypes";
 export const setOnDarkMode = () => {
   return (dispatch) => {
     //dispatch es una funcion que ya viene incluido no hay q exportar
-    dispatch({ type: actionsTypes.ON_DARK_MODE }); // es igual a un string que diga 'ON_DARK_MODE'
+    // dispatch({ type: actionsTypes.ON_DARK_MODE }); // es igual a un string que diga 'ON_DARK_MODE'
+     dispatch({ type: actionsTypes.ON_DARK_MODE, payload: true }); // 'ON_DARK_MODE'
+       // se lanza el dispatch al reducer
   };
 };
 
 export const setOffDarkMode = () => {
   return (dispatch) => {
-    dispatch({ type: actionsTypes.OFF_DARK_MODE });
+    // dispatch({ type: actionsTypes.OFF_DARK_MODE });
+    dispatch({ type: actionsTypes.OFF_DARK_MODE, payload: false });
   };
 };
